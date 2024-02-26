ac9586535309b0370a9463b3bde5f650214f88aa
lourdes-miazzo <108072073+lourdes-miazzo@users.noreply.github.com>
2024-02-26 12:07:18 -0300

    Add files via upload

---
+++ src/pages/autogestion/Autogestion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./autogestion.css"
+import Footer from "../../components/footer/Footer.jsx"
+
+const Autogestion = () => {
+  return (
+    <>
+      <div className='backSelfG'></div>
+      <Footer/>
+    </>
+  )
+}
+
+export default Autogestion
